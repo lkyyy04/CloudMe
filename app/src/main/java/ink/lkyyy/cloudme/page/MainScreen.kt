17ad99fc21fd6273e0ab68823ce318dcf7e001d2
lkyyy <909069697@qq.com>
2023-07-31 22:58:39 +0800

    project init.

---
+++ app/src/main/java/ink/lkyyy/cloudme/page/MainScreen.kt
@@ -0,0 +1,18 @@
+package ink.lkyyy.cloudme.page
+
+import androidx.compose.material3.BottomAppBar
+import androidx.compose.runtime.Composable
+
+@Composable
+fun MainScreen(){
+    MyBottomNavigation()
+}
+
+@Composable
+private fun MyBottomNavigation(){
+    BottomAppBar(
+
+    ) {
+
+    }
+}
